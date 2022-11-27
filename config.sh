d61529f2c30a214d7dcda8324870341f1744dbb7
GTPE Student <vkzc4-user@provision.ocp.example.com>
2022-11-27 12:58:04 -0500

    init code

--- config.sh
+++ config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 sudo pip3 install ansible
+sudo pip3 install kubernetes
+sudo pip3 install PyYAML
+sudo pip3 install jsonpatch
+sudo pip3 install openshift
+
+
 
 ansible-playbook  -vv setup-lab-server.yml 
