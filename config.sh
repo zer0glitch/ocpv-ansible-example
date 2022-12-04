dd554d7111c7beb285b58114ab005e3f9e886024
GTPE Student <ttqvs-user@provision.ocp.example.com>
2022-12-03 20:34:52 -0500

    updating config scripts

--- config.sh
+++ config.sh
@@ -5,7 +5,8 @@ sudo pip3 install kubernetes
 sudo pip3 install PyYAML
 sudo pip3 install jsonpatch
 sudo pip3 install openshift
+yum install vim -y
 
 
 
-ansible-playbook  -vv setup-lab-server.yml 
+ansible-playbook  -vv setup-lab-server.yml  -b
