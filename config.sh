caa72287036bf31bc54453be699a01fbdea8318d
cjnovak98 <90428301+cjnovak98@users.noreply.github.com>
2022-12-06 09:24:18 -0500

    Update config.sh
    Changed lines from pip3 installs to dnf installs.

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo pip3 install ansible
-sudo pip3 install kubernetes
-sudo pip3 install PyYAML
-sudo pip3 install jsonpatch
-sudo pip3 install openshift
+sudo dnf install ansible -y
+sudo dnf install python3-kubernetes -y
+sudo dnf install python3-pyyaml -y
+sudo dnf install python3-jsonpatch -y
+sudo dnf install python3-openshift -y
 sudo dnf install vim -y
 sudo ansible-galaxy collection install kubernetes.core
 
