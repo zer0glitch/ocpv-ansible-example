e7533a2afea9985763b0f75fe87915fc6d58adff
GTPE Student <5fpzh-user@provision.ocp.example.com>
2022-12-08 10:37:25 -0500

    updates for 4.10

--- config.sh
+++ config.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 sudo pip3 install -U pip
-sudo pip3 install ansible
-sudo pip3 install kubernetes
-sudo pip3 install pyyaml 
+
+sudo dnf install -y https://vault.centos.org/centos/8/extras/x86_64/os/Packages/centos-release-ansible-29-1-2.el8.noarch.rpm
+
+sudo dnf install -y https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/p/python3-kubernetes-11.0.0-6.el8.noarch.rpm
+
+sudo dnf install -y https://vault.centos.org/centos/8/BaseOS/x86_64/os/Packages/python3-pyyaml-3.12-12.el8.x86_64.rpm
+
+sudo dnf install -y https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/p/python3-openshift-0.11.2-1.el8.noarch.rpm
+
+#sudo pip3 install ansible
+#pip3 install kubernetes
+#pip3 install pyyaml 
 sudo dnf install python3-jsonpatch -y
-sudo pip3 install openshift 
+#pip3 install openshift 
 sudo dnf install vim -y
-sudo ansible-galaxy collection install kubernetes.core
+ansible-galaxy collection install kubernetes.core
 
 ansible-playbook  -vv setup-lab-server.yaml  -b
