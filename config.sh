63a99fbff0f2cab68882a75d50fb17844162b4dd
GTPE Student <dmbxq-user@provision.ocp.example.com>
2022-12-20 22:24:03 -0500

    refactoring for demo

--- config.sh
+++ config.sh
@@ -2,8 +2,9 @@
 
 sudo pip3 install -U pip
 sudo pip3 install ansible
+sudo pip3 install kubernetes
 
 ansible-playbook  -vv setup-ocpv-ansible-enviornment.yaml  -b
-#ansible-playbook  -vv setup-ocpv-user.yaml
-ansible-playbook  -vv setup-lab-server.yaml  -b
-ansible-playbook  -vv deploy-cnv.yaml 
+ansible-playbook  -vv setup-ocpv-user.yaml
+#ansible-playbook  -vv setup-lab-server.yaml  -b
+#ansible-playbook  -vv deploy-cnv.yaml 
