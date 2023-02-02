51a727e78adc4325c4b36d999f3125f27d5657b4
jamie.whetsell <jwhetsel@redhat.com>
2023-02-01 23:19:03 -0500

    adding aap documentation

--- config.sh
+++ config.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo dnf install podman -y
-cd aap
+sudo pip3 install -U pip
+sudo pip3 install ansible==2.10
+sudo pip3 install kubernetes
+sudo pip3 install openshift
+sudo pip3 install jsonpath
+sudo pip3 install -U kubernetes==12.0.0
+#sudo pip3 install -U pyyml
 
-#ansible-navigator run examples/deploy-cnv.yml -m stdout --eei localhost/zer0glitch.ocpv:1.0.0
+ansible-playbook  -vv examples/setup-ocpv-ansible-enviornment.yml  -b
+ansible-playbook  -vv examples/setup-ocpv-user.yml
+ansible-playbook  -vv examples/setup-lab-server.yml  -b
+#ansible-playbook  -vv deploy-cnv.yml 
